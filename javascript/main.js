75e51ce8b8e85fa943aabad03308b96737e5f174
NatalieNND <nataliedaniels904@gmail.com>
2018-11-26 09:13:31 +0200

    new updated CV

---
+++ javascript/main.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML
+    = "I am a future web developer who is passionate about working with computers Hardwork and Dedication are my 2 main qualities when i try out something new that i want to pursue in. I am a very outgoing person who loves spending time outdoors and enjoying nature. I have a passion for fashion so i love designing and drawing and i am always looking for different ways to put my creativity to the test";
+}
