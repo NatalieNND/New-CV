a9d9f2f87f218de039f344296bca1260bb4cd371
NatalieNND <nataliedaniels904@gmail.com>
2018-12-06 12:29:58 +0200

    added new background image and styled various sections

--- javascript/main.js
+++ javascript/main.js
@@ -3,10 +3,4 @@ function myFunction() {
     = "I am a future web developer who is passionate about working with computers Hardwork and Dedication are my 2 main qualities when i try out something new that i want to pursue in. I am a very outgoing person who loves spending time outdoors and enjoying nature. I have a passion for fashion so i love designing and drawing and i am always looking for different ways to put my creativity to the test";
 }
 
-function changeText(id) {
-    id.innerHTML = "081 053 9007";
-}
 
-function changeText(id) {
-    id.innerHTML = "nataliedaniels904@gmail.com"
-}
