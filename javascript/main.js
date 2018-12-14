3572bd0fdffbe652718c28ea32d44d5963a61072
NatalieNND <nataliedaniels904@gmail.com>
2018-12-14 13:38:25 +0200

    made styling and content changes

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 function myFunction() {
     document.getElementById("demo").innerHTML
-    = "I am a future web developer who is passionate about working with computers Hardwork and Dedication are my 2 main qualities when i try out something new that i want to pursue in. I am a very outgoing person who loves spending time outdoors and enjoying nature. I have a passion for fashion so i love designing and drawing and i am always looking for different ways to put my creativity to the test";
+    = "I am a Junior Web Developer who is passionate about working with computers. Hardwork and dedication are my two main qualities when I try out something new that I want to pursue. I am a very outgoing person who loves spending time outdoors and enjoying nature. I have a passion for fashion so I love designing and drawing and I am always looking for different ways to put my creativity to the test.";
 }
 
 
